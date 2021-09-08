b2410b57f446da4bdfc4484f39454f33c401ce07
Valentin Darricau <darricau.valentin@gmail.com>
2021-09-08 22:05:22 +1000

    remove space + useless import

--- src/Form/WorkoutFormType.php
+++ src/Form/WorkoutFormType.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Form;
 
-use App\Entity\Component;
 use App\Entity\Workout;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
