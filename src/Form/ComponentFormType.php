971f5dcf713cb24b7344f9ac1db9bcaa9b788606
Valentin Darricau <darricau.valentin@gmail.com>
2021-09-30 21:55:09 +1000

    [WORKOUT] add autocomplete package + remove google autocomplete from exercises

--- src/Form/ComponentFormType.php
+++ src/Form/ComponentFormType.php
@@ -7,19 +7,21 @@ namespace App\Form;
 use App\Entity\Component;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ComponentFormType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'autocomplete' => 'off',
+                ]
+            ])
             ->add('setAndRep')
-//            ->add('variation', EntityType::class, [ // We'll come back to that later
-//                'class' => Variation::class,
-//            ])
             ->add('orderNumber', HiddenType::class, [
                 'attr' => [
                     'data-workout-form-target' => 'orderNumber',
